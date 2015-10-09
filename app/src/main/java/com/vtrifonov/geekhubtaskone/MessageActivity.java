4519fe537a47b13a73ed8664956eb1e37154ca55
volfor <volfor@mail.ua>
2015-10-09 20:58:21 +0300

    initial commit

---
+++ app/src/main/java/com/vtrifonov/geekhubtaskone/MessageActivity.java
@@ -0,0 +1,22 @@
+package com.vtrifonov.geekhubtaskone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MessageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        TextView helloMessage = (TextView) findViewById(R.id.view_message);
+        String s = getIntent().getStringExtra("msg");
+        if (s.trim().length() > 0)
+            helloMessage.setText(s);
+        else
+            helloMessage.setText(R.string.hello_world);
+    }
+
+}
